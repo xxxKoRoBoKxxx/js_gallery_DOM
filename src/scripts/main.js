7f1c62b6d1e71763b1d01b8ef903fc56303e2fa7
Andrii <andreyfil1337@gmail.com>
2025-03-23 19:27:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+function changeLargeImg(e) {
+  if (!(e instanceof Event)) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const target = e.target;
+  let source = '';
+
+  if (target.matches('.list-item__link')) {
+    source = target.href;
+  } else if (target.matches('.gallery__thumb')) {
+    source = target.parentElement.href;
+  }
+
+  largeImg.setAttribute('src', source);
+}
+
+thumbs.addEventListener('click', changeLargeImg);
